892a5b14ba7f84c8dfa4eba493798079f7827b4a
CM Student <cmstudent@UHWO-ACM-18.local>
2019-02-21 10:29:27 -1000

    commit

---
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var fruitNames:[String]
+
+fruitNames = ["Kiwi","Apples","Watermelon"]
+print(fruitNames)
+
+print("I like to eat \(fruitNames)")
+
+for name in fruitNames{
+    print("I like to eat " + name)
+
+}
+print(fruitNames[1])
+print(fruitNames[0])
+
+//Part 8, Start of Assignment #4
+
+var numbers:[String]
+
+numbers = ["0","1","2","7","8"]
+print(numbers)
+
+print("My favorite number is\(numbers)")
+
+for name in numbers{
+    print("My favorite number is " + name)
+}
+print(numbers[1])
